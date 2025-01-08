38d7a8fe10b7c0dd371e51842149fe2e1cf77f7a
Mattia Ferrarini <mattiaferrarini22@gmail.com>
2025-01-08 22:41:06 +0100

    Add logo

--- MEMEAirlinesClient/src/App.vue
+++ MEMEAirlinesClient/src/App.vue
@@ -3,9 +3,12 @@
 
 <template>
   <div>
-    <nav class="bg-blue-500 p-4 flex justify-center">
+    <nav class="bg-blue-500 p-4 flex justify-center shadow-md">
       <router-link to="/">
-        <h1 class="cursor-pointer text-white text-2xl">MEME Airlines</h1>
+        <div class="flex gap-2 items-center justify-center">
+          <img src="/logo.png" alt="MEME Airlines logo" class="w-10 h-10"/>
+          <h1 class="cursor-pointer text-white text-2xl font-semibold">MEME Airlines</h1>
+        </div>
       </router-link>
     </nav>
     <RouterView/>
